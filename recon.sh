c2d01e7ccaf25495a9114b9680d296929c05948b
sourabhtomar <88035485+sourabhtomar@users.noreply.github.com>
2024-11-18 11:30:10 +0530

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -20,23 +20,26 @@ sublist3r -d $1 | tee ~/bugbounty_targets/$1/sublist3r_subdomains
 
 echo "Gathering subdomains from amass"
 
-amass enum --passive -d $1 | tee ~/bugbounty_targets/$1/amass_subdomains
+#amass enum --passive -d $1 | tee ~/bugbounty_targets/$1/amass_subdomains
 
 echo "Gathering subdomains from crt.sh"
 
 curl -s https://crt.sh/\?q\=\%.$1\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee ~/bugbounty_targets/$1/crt.txt
 
+echo "Gathering subdomains from crt tool"
+bash ~/tools/crt.sh/crt.sh -d $1 | tee ~/bugbounty_targets/$1/crt2.txt
+
 cd ~/bugbounty_targets/$1
 
 echo "Filtering subdomains to unique"
 
-cat amass_subdomains sublist3r_subdomains findomain_subdomains subfinder_subdomains assetfinder_subdomains crt.txt | sort -u | tee unique_subdomains.txt
+cat amass_subdomains sublist3r_subdomains findomain_subdomains subfinder_subdomains assetfinder_subdomains crt.txt crt2.txt | sort -u | tee unique_subdomains.txt
 
-rm amass_subdomains sublist3r_subdomains findomain_subdomains subfinder_subdomains assetfinder_subdomains crt.txt
+rm amass_subdomains sublist3r_subdomains findomain_subdomains subfinder_subdomains assetfinder_subdomains crt.txt crt2.txt
 
 echo "Finding live subdomains"
 
-cat unique_subdomains.txt | httpx | tee alive_subdomains
+cat unique_subdomains.txt | httpx -silent -sc -probe -title -td -ip -t 90 -mc 200,404,403,302,301,303,304,305,306,307,302 | tee alive_subdomains
 
 echo "Taking screenshot"
 cat alive_subdomains | aquatone -out aquatone
